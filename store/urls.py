c5e3ecde1f3184619c84242ebadff3aa1ba2d6da
ondrejhonus <ondrejhonus@proton.me>
2025-04-30 19:26:34 +0200

    Categories of instruments + pages for them

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('strings/', views.strings, name='strings'),
+    path('percussion/', views.percussion, name='percussion'),
+    path('keyboards/', views.keyboards, name='keyboards'),
+    path('wind-instruments/', views.wind, name='wind'),
 ]
--- store/views.py
+++ store/views.py
@@ -9,3 +9,31 @@ def index(request):
         'instruments': instruments
         }
     return render(request, 'index.html', context=context)
+
+def strings(request):
+    instruments = Instrument.objects.filter(category__name='Strings')
+    context = {
+        'instruments': instruments
+        }
+    return render(request, 'categories/strings/all.html', context=context)
+
+def percussion(request):
+    instruments = Instrument.objects.filter(category__name='Percussion')
+    context = {
+        'instruments': instruments
+        }
+    return render(request, 'categories/percussion/all.html', context=context)
+
+def keyboards(request):
+    instruments = Instrument.objects.filter(category__name='Keyboards')
+    context = {
+        'instruments': instruments
+        }
+    return render(request, 'categories/keyboards/all.html', context=context)
+
+def wind(request):
+    instruments = Instrument.objects.filter(category__name='Wind')
+    context = {
+        'instruments': instruments
+        }
+    return render(request, 'categories/wind/all.html', context=context)
