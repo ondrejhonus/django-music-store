e55aaf47d70bc942ef79ea6e6f0b4786c20cfa39
ondrejhonus <ondrejhonus@proton.me>
2025-04-30 22:03:54 +0200

    pages for types

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('strings/', views.strings, name='strings'),
+    path('strings/<str:type>/', views.string_type, name='strings'),
     path('percussion/', views.percussion, name='percussion'),
+    path('percussion/<str:type>/', views.percussion_type, name='percussion'),
     path('keyboards/', views.keyboards, name='keyboards'),
+    path('keyboards/<str:type>/', views.keyboard_type, name='keyboards'),
     path('wind-instruments/', views.wind, name='wind'),
+    path('wind-instruments/<str:type>/', views.wind_type, name='wind-instruments'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import Instrument
+from .models import Instrument, InstrumentCategory, InstrumentType
 
 # Create your views here.
 
 def index(request):
-    instruments = Instrument.objects.all().order_by('year')
+    instruments = Instrument.objects.all().order_by('year').reverse()
     context = {
         'instruments': instruments
         }
@@ -17,6 +17,16 @@ def strings(request):
         }
     return render(request, 'categories/strings/all.html', context=context)
 
+def string_type(request, type):
+    if not InstrumentType.objects.filter(name=type).exists():
+        return render(request, '404.html')
+    instruments = Instrument.objects.filter(type__name=type)
+    context = {
+        'instruments': instruments,
+        'type': type,
+        }
+    return render(request, 'categories/strings/type.html', context=context)
+
 def percussion(request):
     instruments = Instrument.objects.filter(type__category='2')
     context = {
@@ -24,12 +34,32 @@ def percussion(request):
         }
     return render(request, 'categories/percussion/all.html', context=context)
 
+def percussion_type(request, type):
+    if not InstrumentType.objects.filter(name=type).exists():
+        return render(request, '404.html')
+    instruments = Instrument.objects.filter(type__name=type)
+    context = {
+        'instruments': instruments,
+        'type': type,
+        }
+    return render(request, 'categories/percussion/type.html', context=context)
+
 def keyboards(request):
     instruments = Instrument.objects.filter(type__category='3')
     context = {
-        'instruments': instruments
+        'instruments': instruments,
         }
     return render(request, 'categories/keyboards/all.html', context=context)
+
+def keyboard_type(request, type):
+    if not InstrumentType.objects.filter(name=type).exists():
+        return render(request, '404.html')
+    instruments = Instrument.objects.filter(type__name=type)
+    context = {
+        'instruments': instruments,
+        'type': type,
+        }
+    return render(request, 'categories/keyboards/type.html', context=context)
 
 def wind(request):
     instruments = Instrument.objects.filter(type__category='4')
@@ -37,3 +67,13 @@ def wind(request):
         'instruments': instruments
         }
     return render(request, 'categories/wind/all.html', context=context)
+
+def wind_type(request, type):
+    if not InstrumentType.objects.filter(name=type).exists():
+        return render(request, '404.html')
+    instruments = Instrument.objects.filter(type__name=type)
+    context = {
+        'instruments': instruments,
+        'type': type,
+        }
+    return render(request, 'categories/wind/type.html', context=context)
