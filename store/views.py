194f09bff351ad7c52ff31c49b2540caceca485c
ondrejhonus <honusondrej@gmail.com>
2025-05-01 12:22:06 +0200

    universal functions, breadcrumb to detail

--- store/views.py
+++ store/views.py
@@ -5,8 +5,32 @@ def index(request):
     instruments = Instrument.objects.all().order_by('year').reverse()
     context = {
         'instruments': instruments,
+        'categories': InstrumentCategory.objects.all(),
         }
     return render(request, 'index.html', context=context)
+
+def instrument(request, category_slug):
+    instruments = Instrument.objects.all()
+    context = {
+        'instruments': instruments.filter(type__category__slug=category_slug),
+        'category': InstrumentCategory.objects.get(slug=category_slug),
+        'types': InstrumentType.objects.filter(category__slug=category_slug),
+        }
+    return render(request, f'categories/all.html', context=context)
+
+def instrument_type(request, category_slug, type_slug):
+    if not InstrumentCategory.objects.filter(slug=category_slug).exists():
+        return render(request, '404.html')
+    if not InstrumentType.objects.filter(slug=type_slug, category__slug=category_slug).exists():
+        return render(request, '404.html')
+    instruments = Instrument.objects.filter(type__slug=type_slug, type__category__slug=category_slug)
+    context = {
+        'instruments': instruments,
+        'type': InstrumentType.objects.get(slug=type_slug).name,
+        'types': InstrumentType.objects.filter(category__slug=category_slug),
+        'category': InstrumentCategory.objects.get(slug=category_slug).name,
+        }
+    return render(request, f'categories/type.html', context=context)
 
 def instrument_detail(request, cat, slug):
     if not InstrumentType.objects.filter(category__slug=cat).exists():
@@ -17,91 +41,7 @@ def instrument_detail(request, cat, slug):
     context = {
         'instrument': instrument,
         'category': InstrumentCategory.objects.get(slug=cat),
+        'type': InstrumentType.objects.get(slug=instrument.type.slug),
         }
     return render(request, 'instruments/detail.html', context=context)
 
-
-##################################################
-################### STRINGS ######################
-##################################################
-
-def strings(request):
-    instruments = Instrument.objects.filter(type__category='1')
-    context = {
-        'instruments': instruments,
-        }
-    return render(request, 'categories/strings/all.html', context=context)
-
-def string_type(request, type):
-    if not InstrumentType.objects.filter(slug=type).exists():
-        return render(request, '404.html')
-    instruments = Instrument.objects.filter(type__slug=type)
-    context = {
-        'instruments': instruments,
-        'type': InstrumentType.objects.get(slug=type).name,
-        }
-    return render(request, 'categories/strings/type.html', context=context)
-
-###############################################
-################# PERCUSSION ###################
-###############################################
-
-def percussion(request):
-    instruments = Instrument.objects.filter(type__category='2')
-    context = {
-        'instruments': instruments
-        }
-    return render(request, 'categories/percussion/all.html', context=context)
-
-def percussion_type(request, type):
-    if not InstrumentType.objects.filter(slug=type).exists():
-        return render(request, '404.html')
-    instruments = Instrument.objects.filter(type__slug=type)
-    context = {
-        'instruments': instruments,
-        'type': InstrumentType.objects.get(slug=type).name,
-        }
-    return render(request, 'categories/percussion/type.html', context=context)
-
-
-################################################
-################# KEYBOARDS ####################
-################################################
-
-def keyboards(request):
-    instruments = Instrument.objects.filter(type__category='3')
-    context = {
-        'instruments': instruments,
-        }
-    return render(request, 'categories/keyboards/all.html', context=context)
-
-def keyboard_type(request, type):
-    if not InstrumentType.objects.filter(slug=type).exists():
-        return render(request, '404.html')
-    instruments = Instrument.objects.filter(type__slug=type)
-    context = {
-        'instruments': instruments,
-        'type': InstrumentType.objects.get(slug=type).name,
-        }
-    return render(request, 'categories/keyboards/type.html', context=context)
-
-###############################################
-################# WIND #########################
-###############################################
-
-def wind(request):
-    instruments = Instrument.objects.filter(type__category='4')
-    context = {
-        'instruments': instruments
-        }
-    return render(request, 'categories/wind/all.html', context=context)
-
-def wind_type(request, type):
-    if not InstrumentType.objects.filter(slug=type).exists():
-        return render(request, '404.html')
-    instruments = Instrument.objects.filter(type__slug=type)
-    context = {
-        'instruments': instruments,
-        'type': InstrumentType.objects.get(slug=type).name,
-        }
-    return render(request, 'categories/wind/type.html', context=context)
